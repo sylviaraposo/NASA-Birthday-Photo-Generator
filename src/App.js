b1584c9b1b3aad5aec451c0fd9c24846eeadc5a2
sylvia <sylviaraposo@hotmail.ca>
2022-03-12 14:10:03 -0500

    testing netlify build and safari functionality

--- src/App.js
+++ src/App.js
@@ -43,27 +43,22 @@ function App() {
     event.preventDefault();
     console.log('form submitted');
 
-      // convert the birthdate passed in to a date to validate its within the data
-      try {
-        let chosenBirthdateParsed = new Date(Date.parse((chosenBirthdate).replace(/-/g, " ")))
-        let earliestDatabaseDate = new Date('June 16, 1995')
+    // convert the birthdate passed in to a date to validate its within the data
+    let chosenBirthdateParsed = new Date(Date.parse((chosenBirthdate).replace(/-/g, "/")))
+    let earliestDatabaseDate = new Date('June 16, 1995')
 
-      
+  
 
-      // A conditional statement to check if the birthdate selected is before a date available in the database, if so the application will display the most recent picture for the birthday submitted for the earliest year available, else set the original input to original chosenBirthdate
-      if(chosenBirthdateParsed <= earliestDatabaseDate) {
-        chosenBirthdateParsed.setFullYear(1996);
-        let chosenBirthdateString = chosenBirthdateParsed.toISOString().split('T')[0];
-        console.log(chosenBirthdateString);
-        setUserBirthdate(chosenBirthdateString);
-      } else {
-        setUserBirthdate(chosenBirthdate);
-      }
+    // A conditional statement to check if the birthdate selected is before a date available in the database, if so the application will display the most recent picture for the birthday submitted for the earliest year available, else set the original input to original chosenBirthdate
+    if(chosenBirthdateParsed <= earliestDatabaseDate) {
+      chosenBirthdateParsed.setFullYear(1996);
+      let chosenBirthdateString = chosenBirthdateParsed.toISOString().split('T')[0];
+      console.log(chosenBirthdateString);
+      setUserBirthdate(chosenBirthdateString);
+    } else {
+      setUserBirthdate(chosenBirthdate);
     }
-    catch (error) {
-      alert(error);
-      alert('beep');
-    }
+  
   }
   // Render the application
   return (
