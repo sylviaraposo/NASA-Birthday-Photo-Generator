1f8bca188eb791a44e9bfce477eaf5a64b94c08d
sylvia <sylviaraposo@hotmail.ca>
2022-03-17 07:38:30 -0400

    modidified footer links;

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,9 @@ import './App.css'
 function Footer() {
     return (
         <footer>
-            <p>API Powered by <a href="https://api.nasa.gov/" target="_blank">NASA</a> 🚀 
-                <span className="text-break">Made by <a href="https://sylviaraposo.com" target="_blank">Sylvia</a></span> 🚀
-                <span className="text-break"> Made at <a href="https://junocollege.com/" target="_blank">Juno College</a></span></p>
+            <p>API Powered by <a href="https://api.nasa.gov/" target="_blank" rel="noreferrer">NASA</a> 🚀 
+                <span className="text-break">Made by <a href="https://sylviaraposo.com" target="_blank" rel="noreferrer">Sylvia</a></span> 🚀
+                <span className="text-break"> Made at <a href="https://junocollege.com/" target="_blank" rel="noreferrer">Juno College</a></span></p>
         </footer>
     )
 }
