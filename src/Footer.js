d6e7ae590eb0fb79fda921cebabad80bf0b26e05
sylvia <sylviaraposo@hotmail.ca>
2022-05-04 13:10:54 -0400

    editing mobile styling

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer>
             <p>API Powered by <a href="https://api.nasa.gov/" target="_blank" rel="noreferrer">NASA</a> 🚀 
-                <span className="text-break">Made by <a href="https://sylviaraposo.com" target="_blank" rel="noreferrer">Sylvia</a></span> 🚀
+                <span className="text-break">Made by <a href="https://sylviaraposo.com" target="_blank" rel="noreferrer">Sylvia🚀</a></span> 
                 <span className="text-break"> Made at <a href="https://junocollege.com/" target="_blank" rel="noreferrer">Juno College</a></span></p>
         </footer>
     )
